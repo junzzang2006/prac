90ba18ceee0ac504bee2b610904a61b258e6faf1
junzzang2006 <junzzang2006@gmail.com>
2025-07-05 23:59:04 +0900

    7/5(78p까지)

---
+++ src/main/java/com/example/prac/Answer.java
@@ -0,0 +1,27 @@
+package com.example.prac;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+public class Answer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @ManyToOne
+    private Question question;
+
+}
